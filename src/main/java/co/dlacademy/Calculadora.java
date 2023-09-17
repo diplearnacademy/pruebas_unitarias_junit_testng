a33eed0ec56f464e41ea5f542c2eb48c49b98fdc
Bryan Estefan Moreno Diaz <estefandx@hotmail.com>
2023-09-17 15:22:12 -0500

    prueba unitaria calculadora

---
+++ src/main/java/co/dlacademy/Calculadora.java
@@ -0,0 +1,7 @@
+package co.dlacademy;
+
+public class Calculadora {
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/co/dlacademy/PruebasUnitarias.java
@@ -0,0 +1,13 @@
+package co.dlacademy;
+
+import org.junit.jupiter.api.Test;
+
+public class PruebasUnitarias {
+
+    @Test
+    void testTituloDeLaPrueba(){
+        // arrange
+        // act
+        // assert
+    }
+}
