36b93c48962de603f9b26568367cb36ad49915a4
Bryan Estefan Moreno Diaz <estefandx@hotmail.com>
2023-09-21 17:02:56 -0500

    base junit

---
+++ src/test/java/co/dlacademy/ProductoTestParametros.java
@@ -0,0 +1,82 @@
+package co.dlacademy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class ProductoTestParametros {
+
+    private static Stream<Arguments> productos() {
+        return Stream.of(
+                arguments("producto1", 10.0,10),
+                arguments("producto2", 6.8),
+                arguments("producto3", 27.8),
+                arguments("producto4", 89.8)
+        );
+
+    }
+
+    private static Stream<Arguments> productosObjeto() {
+        return Stream.of(
+                arguments(new Producto("tenis",10.0),"tenis"),
+                arguments(new Producto("medias",89.0),"medias"),
+                arguments(new Producto("saco",78.0),"saco"),
+                arguments(new Producto("chaqueta",45.0),"chaqueta")
+        );
+
+    }
+
+
+
+
+    @ParameterizedTest
+    @MethodSource("productosObjeto")
+     void testObtenerNombreObjeto(Producto producto,String valorEsperado) {
+
+        assertEquals(valorEsperado, producto.getNombre());
+    }
+
+
+
+
+    @ParameterizedTest
+    @MethodSource("productos")
+    public void testObtenerNombreParametros(String nombreProducto,double precio) {
+        Producto producto = new Producto(nombreProducto, precio);
+        assertEquals(nombreProducto, producto.getNombre());
+    }
+
+    @Test
+    public void testObtenerPrecio() {
+        Producto producto = new Producto("Producto 2", 15.0);
+        assertEquals(15.0, producto.getPrecio(), 0.01);
+    }
+
+    @Test
+    public void testProductoSinNombre() {
+        assertThrows(IllegalArgumentException.class, () -> new Producto("", 25.0));
+        assertThrows(IllegalArgumentException.class, () -> new Producto(null, 25.0));
+    }
+
+    @Test
+    public void testProductoPrecioNegativo() {
+        assertThrows(IllegalArgumentException.class, () -> new Producto("Producto 2", 0));
+    }
+
+
+
+
+}
+
